edd9082eb8e11009b70538aad22b12a0e97c177d
Tomasz Knapik <me@tmkn.org>
2018-07-06 23:36:08 +0100

    Improve streamfield

--- tmknorg/core/blocks.py
+++ tmknorg/core/blocks.py
@@ -9,6 +9,6 @@ class ImageBlock(blocks.StructBlock):
 
 class BodyBlock(blocks.StreamBlock):
     paragraph = blocks.RichTextBlock()
-    h2 = blocks.CharBlock()
-    h3 = blocks.CharBlock()
-    image = ImageBlock()
+    h2 = blocks.CharBlock(template='core/blocks/h2.html')
+    h3 = blocks.CharBlock(template='core/blocks/h3.html')
+    image = ImageBlock(template='core/blocks/image.html')
