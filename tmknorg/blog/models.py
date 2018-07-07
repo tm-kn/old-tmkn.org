b375b2695b5f86c2c6a8987b4fdc81a914f439c1
Tomasz Knapik <me@tmkn.org>
2018-07-07 08:00:10 +0100

    Add pagination

--- tmknorg/blog/models.py
+++ tmknorg/blog/models.py
@@ -1,3 +1,7 @@
+from django.core.paginator import (EmptyPage, InvalidPage, PageNotAnInteger,
+                                   Paginator)
+from django.shortcuts import redirect
+
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
@@ -9,11 +13,23 @@ class BlogIndex(Page):
     parent_page_types = ['home.HomePage']
     subpage_types = ['blog.BlogPage']
 
+    def serve(self, request, *args, **kwargs):
+        try:
+            return super().serve(request, *args, **kwargs)
+        except (EmptyPage, PageNotAnInteger, InvalidPage):
+            return redirect(self.relative_url(
+                getattr(request, 'site', None),
+                request=request
+            ))
+
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        context['blog_pages'] = (
+        qs = (
             self.get_children().live().public().order_by('-first_published_at')
         )
+        paginator = Paginator(qs, 20)
+        page = request.GET.get('page', 1)
+        context['blog_pages'] = paginator.page(page)
         return context
 
 
